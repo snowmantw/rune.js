e1aee3ec9fc2c70439cb15b4099b6c040abed2b8
Greg Weng <snowmantw@gmail.com>
2015-09-17 00:22:49 +0800

    prepare to test Edda with switcher and queue

--- demo/edda.runtime.js
+++ demo/edda.runtime.js
@@ -30,6 +30,13 @@ Runtime.prototype.done = function() {
 };
 
 Runtime.prototype.run = function(signal) {
+  var deferred = new Runtime.Deferred();
+  this._queue = deferred.promise;
+  this._resolve = deferred.resolve;
+  this._reject = deferred.reject;
+
+  // Everytime these input sources have new inputs, they
+  // will push one new handler into the queue, and execute it.
   this._generators.forEach((generator) => {
     generator.onchange(this._pulse.bind(this));
   });
@@ -37,22 +44,28 @@ Runtime.prototype.run = function(signal) {
 
 /**
  * Receive one nativeEvent OR signal.
- * TODO: QUEUING ONLY NATIVE EVENTS.
+ * The handler will be queued, and only when the
+ * main queue is not interrupted, the task will be
+ * executed.
  */
 Runtime.prototype._pulse = function(change) {
-  var signal;
-  if (!(change instanceof Signal)) {
-    // Turn the native event to the signal.
-    var data = change;
-    signal = new Signal(change.type, data);
-  } else {
-    // The change is already a signal.
-    signal = change;
-  }
+  this._queue = this._queue.then(() => {
+    var signal;
+    if (!(change instanceof Signal)) {
+      // Turn the native event to the signal.
+      var data = change;
+      signal = new Signal(change.type, data);
+    } else {
+      // The change is already a signal.
+      signal = change;
+    }
 
-  var { state, effect } = this._route(signal);
-  if (!state) { return; }   // no corresponding one.
-  this._kickoff(signal, state, effect);
+    var { state, effect } = this._route(signal);
+    if (!state) { return; }   // no corresponding one.
+    // _kickoff may interrupt the queue, if the result contains an Event.
+    this._kickoff(signal, state, effect);
+  });
+  this._queue = this._queue.catch(this._onQueueError.bind(this));
 };
 
 Runtime.prototype._kickoff = function(currentsig ,state, effect) {
@@ -73,9 +86,41 @@ Runtime.prototype._kickoff = function(currentsig ,state, effect) {
 };
 
 Runtime.prototype._onEvent = function(signal, event) {
-  
+  var newInstance = this._switcher(event);
+  // And then interrupt all the following handlers of signals and
+  // native events.
+  this._interrupt();
+  // Finally, switch to the next instance with the signal.
+  newInstance.run(signal);
 };
 
-Runtime.ptototype._onSignal = function(signal) {
-
+Runtime.prototype._interrupt = function() {
+  var interrupt = new Runtime.Interrupt();
+  this._reject(interrupt);
 };
+
+Runtime.prototype._onSignal = function(signal) {
+  this._pulse(signal);
+};
+
+Runtime.prototype._onQueueError = function(err) {
+  if (!(err instanceof Runtime.Interrupt)) {
+    // Print it to debug.
+    console.error(err);
+    // Then to interrupt the queue.
+    throw err;
+  } else {
+    // Only to interrupt the queue.
+  }
+};
+
+Runtime.Deferred = function() {
+  var promise = new Promise((resolve, reject) => {
+    this.resolve = resolve;
+    this.reject = reject;
+  });
+  this.promise = promise;
+  return this;
+};
+
+Runtime.Interrupt = function() {};
